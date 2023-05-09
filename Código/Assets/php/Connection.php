414df7a62a7020e343dcfb67b2c02284f3c0c7de
Alex P <90261293+alexator20@users.noreply.github.com>
2023-05-09 10:42:39 +0200

    php

---
+++ Código/Assets/php/Connection.php
@@ -0,0 +1,24 @@
+<?php
+
+class Connection
+{
+    protected PDO $conn;
+    protected function connect()
+    {
+        $config = json_decode(file_get_contents(__DIR__ . "../js/dba.json"), true);
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $config["host"] . ";dbname=" . $config["dbname"] . ";port=" . $config["port"],
+                $config["user"],
+                $config["password"]
+            );
+        } catch (PDOException $exception) {
+            echo "ha fallado la conexión. Error: " . $exception->getMessage();
+        }
+    }
+
+    public function __destruct()
+    {
+        if ($this->conn)  $this->conn = null;
+    }
+}
