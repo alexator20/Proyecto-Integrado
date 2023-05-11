7a8efb958c8a61c7b2e1dc84758a857ae57cc342
kikso <quiquevolc@gmail.com>
2023-05-11 13:17:50 +0200

    clase producto creada

--- Código/Assets/php/classes/Product.php
+++ Código/Assets/php/classes/Product.php
@@ -1,5 +1,18 @@
 <?php
 
 class Product {
+
+    private int $cod_producto;
+    private string $nombre;
+    private string $precio;
+    private string $categoria;
+
+    public function __construct(int $cod_producto, string $nombre,string $precio, string $categoria)
+    {
+        $this->cod_producto = $cod_producto;
+        $this->nombre = $nombre;
+        $this->precio = $precio;
+        $this->categoria = $categoria;
+    }
     
 }
