78c08f084a4108518d6e347b6f2ac7d262303477
kikso <quiquevolc@gmail.com>
2023-05-11 10:16:33 +0200

    Nueva funcion de get

--- Código/Assets/php/productRepository.php
+++ Código/Assets/php/productRepository.php
@@ -9,7 +9,24 @@ class productRepository extends Connection{
 
     public function getAllProduct():array 
     {
-        $stmt = $this->conn->query("SELECT * FROM movement ORDER BY cod_producto DESC");
+        $stmt = $this->conn->query("SELECT * FROM producto ORDER BY cod_producto DESC");
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $data[] = new Product(...$row);
+        }
+        return $data;
+    }
+
+    public function getProduct(int $id): Product
+    {
+        $stmt = $this->conn->query("SELECT * FROM producto WHERE cod_producto = $id");
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return new Product(...$row);
+    }
+
+    public function getAllCategoryProduct(string $category):array 
+    {
+        $stmt = $this->conn->query("SELECT * FROM producto WHERE categoria = $category ORDER BY cod_producto DESC");
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             $data[] = new Product(...$row);
         }
@@ -17,6 +34,10 @@ class productRepository extends Connection{
     }
 
     
+
+
+
+
 
 
 }
