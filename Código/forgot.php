b1730d79acaf3cc5c47c6f954c81853df1a9a906
XxAdriianxX~ <adrigonzalgand@gmail.com>
2023-05-15 11:41:10 +0200

    e

--- Código/forgot.php
+++ Código/forgot.php
@@ -18,7 +18,7 @@ if (isset($_POST["forgot_password"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register y log in</title>
+    <title>Forgot password</title>
     <link rel="stylesheet" href="./Assets/css/register.css">
 </head>
 
