37fe2e8e89b6cc5cb8af3d47aa16ecce30819fa7
Alex P <90261293+alexator20@users.noreply.github.com>
2023-05-17 09:06:36 +0200

    update register

--- Codigo/register.php
+++ Codigo/register.php
@@ -1,8 +1,7 @@
 <?php
 require_once __DIR__ . "/php/autoloader.php";
-
 $security = new Security();
-$security->checkLoggedIn();
+$security->doLogin();
 ?>
 
 
@@ -40,7 +39,7 @@ $security->checkLoggedIn();
                     <input type="password" name="password"  minlength="6" maxlength="20" value="" required>
                 </label>
                 <a href="./forgot.php"><p class="forgot-pass">Forgot password?</p></a>
-                <button type="submit" class="submit" name="login">Sign In</button></a>
+                <input type="submit" class="submit" value="Sign In">
             </form>
         </div>
         <div class="sub-cont">
