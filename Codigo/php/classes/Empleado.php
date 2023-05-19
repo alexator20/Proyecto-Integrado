7e18304859cbb0525b98320546b66a7db4073ef0
Alex P <90261293+alexator20@users.noreply.github.com>
2023-05-19 10:39:54 +0200

    update

--- Codigo/php/classes/Empleado.php
+++ Codigo/php/classes/Empleado.php
@@ -1,6 +1,7 @@
 <?php
 
-class Empleado extends Connection{
+class Empleado extends Connection
+{
 
     private int $cod_empleado;
     private string $nombre;
@@ -11,17 +12,16 @@ class Empleado extends Connection{
     private string $foto;
 
     public function __construct(int $cod_empleado, string $nombre, string $apellido, string $correo, string $direccion, string $contrasenya, string $foto)
-{
-    $this->cod_empleado = $cod_empleado;
-    $this->nombre = $nombre;
-    $this->apellido = $apellido;
-    $this->correo = $correo;
-    $this->direccion = $direccion;
-    $this->contrasenya = $contrasenya;
-    $this->foto = $foto;
-    $this->connect();
-}
-
+    {
+        $this->cod_empleado = $cod_empleado;
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+        $this->correo = $correo;
+        $this->direccion = $direccion;
+        $this->contrasenya = $contrasenya;
+        $this->foto = $foto;
+        $this->connect();
+    }
 
     public function insert(array $data): void
     {
@@ -34,15 +34,10 @@ class Empleado extends Connection{
         $sql = "INSERT INTO empleado
         (nombre,apellidos,correo,direccion,contrasenya) 
         VALUES ('$name','$lastname','$email','$address','$password')";
-        /* echo $sql;
-        die; */
 
         $this->conn->query($sql);
     }
 
-    /**
-     * Get the value of cod_empleado
-     */
     public function getCodEmpleado(): int
     {
         return $this->cod_empleado;
