7c0a16a1039d53b55afefa13c6195d855c5e646c
KiksoS <quiquevolc@gmail.com>
2023-05-19 16:58:18 +0200

    actualizado el if

--- Codigo/mesa.php
+++ Codigo/mesa.php
@@ -2,16 +2,15 @@
 if (session_status() !== PHP_SESSION_ACTIVE) session_start();
 require_once __DIR__ . "/php/autoloader.php";
 
+
 $_SESSION["mesa"] = $_GET["mesa"];
 $_SESSION["categoria"] = "Cafes";
 
 $repository = new TicketRepository;
 
 $checkTicket = $repository->testTicket($_SESSION["idTicket"]);
-if (!isset($_SESSION["idTicket"])) {
 
-    $ticket = $repository->insertTicket($_SESSION["mesa"]);
-} elseif (isset($_SESSION["idTicket"]) && $checkTicket == 0) {
+if (!isset($_SESSION["idTicket"]) || (isset($_SESSION["idTicket"]) && $checkTicket == 0)) {
 
     $ticket = $repository->insertTicket($_SESSION["mesa"]);
 }else {
